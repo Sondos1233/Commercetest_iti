7fb8fb7e979ca4670b4571d7d3c3353083a6378c
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-01-18 00:11:14 +0200

    add Route in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Movies from './Component/Movies/Movie';
+import Form from './Component/Register/form';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+      <Router>
+  
+
+
+      <Navbar />
+      
+        <Switch>
+        <Route path="/register" exact component={Form}/>
+        <Route path="/" exact component={Movies}/>
+ 
+        </Switch>
+
+
+      </Router>
+    </>
   );
 }
 
 export default App;
+

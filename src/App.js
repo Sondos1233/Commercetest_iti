b5f237dd1aa386b37e34e206456cca163970ec1f
Sondos Amgad <69219643+Sondos1233@users.noreply.github.com>
2022-01-18 00:57:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Navbar from './Component/navbar/navbar'
-
+import Moviedetails from './Component/movieDetails.js/movieDetails'
 import Movies from './Component/Movies/Movie';
 import Form from './Component/Register/form';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -22,7 +22,7 @@ function App() {
         <Switch>
         <Route path="/register" exact component={Form}/>
         <Route path="/" exact component={Movies}/>
- 
+         <Route path="/" exact component={Moviedetails}/>
         </Switch>
 
 

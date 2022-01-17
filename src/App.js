00983f63d54dd1749a6c3d23ad67da7f82b2bebc
Sondos Amgad <69219643+Sondos1233@users.noreply.github.com>
2022-01-18 00:50:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
- 
->>>>>>> 1b9bee6f6ceb32cfc3e6d6e0493ebff685582e85
+
 import './App.css';
 import Navbar from './Component/navbar/navbar'
 
@@ -13,7 +10,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-<<<<<<< HEAD
+
     
     <>
       <Router>
@@ -30,10 +27,10 @@ function App() {
 
 
       </Router>
-=======
+
     < >
    
->>>>>>> 1b9bee6f6ceb32cfc3e6d6e0493ebff685582e85
+
     </>
   );
 }
